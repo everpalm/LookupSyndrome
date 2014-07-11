9244cad41a3284a2872625db48a2a15c0b5150c3
丁榮 <Jaron@dingrongde-MacBook-Air.local>
2014-07-12 02:32:36 +0800

    Build base structure

---
+++ LookupSyndrome/LookupSyndrome/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LookupSyndrome
+//
+//  Created by 丁榮 on 2014/7/12.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
